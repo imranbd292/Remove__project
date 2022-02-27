60b4ba457e55c63f9cd0be9d43dfe44c549fdbaa
Imran-804 <imranbd804@gmail.com>
2022-02-27 15:48:48 +0600

    Day-04

---
+++ app.js
@@ -0,0 +1,25 @@
+document.getElementById("circle-1").onclick = function() {
+
+    document.getElementById("circle-1").style.display = "none";
+
+}
+document.getElementById("circle-2").onclick = function() {
+
+    document.getElementById("circle-2").style.display = "none";
+
+}
+document.getElementById("circle-3").onclick = function() {
+
+    document.getElementById("circle-3").style.display = "none";
+
+}
+document.getElementById("circle-4").onclick = function() {
+
+    document.getElementById("circle-4").style.display = "none";
+
+}
+document.getElementById("circle-5").onclick = function() {
+
+    document.getElementById("circle-5").style.display = "none";
+
+}
